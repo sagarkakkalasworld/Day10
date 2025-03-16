4b97f27afc0c60dda5cec9d9db57ceb26ee1fc53
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-03-16 09:04:56 +0000

    Update deploy.sh

--- Bash/deploy.sh
+++ Bash/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-cd /home/ubuntu/bashscripts/Day9/react-helmcharts/
+cd /home/ubuntu/bashscripts/Day10/react-helmcharts/
 aws s3 cp s3://gitcommittagbucket/old_value.txt .
 aws s3 cp s3://gitcommittagbucket/new_value.txt .
 old_value=$(cat old_value.txt)
 new_value=$(cat new_value.txt)
 sed -i "s/${old_value}/${new_value}/g" values.yaml
+git add .
+git commit -m "${new_value} is updated"
+git push https://$GIT_TOKEN@github.com/sagarkakkalasworld/Day10.git
 aws s3 rm s3://gitcommittagbucket/old_value.txt
 echo $new_value > old_value.txt
 aws s3 cp old_value.txt s3://gitcommittagbucket/
